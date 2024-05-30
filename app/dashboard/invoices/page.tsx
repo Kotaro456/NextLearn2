4db7d150415a80448e805581e330f4396d506dc3
Kotaro456 <kotaro03281616@gmail.com>
2024-05-30 12:21:55 +0900

    不要コード削除

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,13 +1,10 @@
-import {fetchRevenue} from "@/app/lib/data";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 
 export default async function Page(){
-    const revenue = await fetchRevenue();
-    console.log(revenue);
     return (
         <>
             <p>invoices page</p>
-            <RevenueChart revenue={revenue}/>
+            <RevenueChart />
         </>
 
     );
